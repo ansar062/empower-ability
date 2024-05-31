7f8de5c86a08c10279c7ca7315803b9fe4dae0fa
Muhammad Ansar <109860407+ansar062@users.noreply.github.com>
2024-06-01 00:02:18 +0500

    Update Jobs.js

--- src/Pages/JobsPage/Jobs.js
+++ src/Pages/JobsPage/Jobs.js
@@ -107,9 +107,9 @@ const JobsPage = () => {
                 onChange={(e) => setSelectedCategory(e.target.value)}
               >
                 <option value="">All Categories</option>
-                <option value="Hearing Impaired">Hearing Impaired</option>
-                <option value="Motor Impaired">Motor Impaired</option>
-                <option value="Speech Impaired">Speech Impaired</option>
+                <option value="hearing impaired">Hearing Impaired</option>
+                <option value="motor impaired">Motor Impaired</option>
+                <option value="speech impaired">Speech Impaired</option>
               </select>
             </label>
           </Styles.FilteringOptions>
